ee50b318cde6c4ba506433fd46eb921911d259f8
Cohen Weston <cweston@student.ubc.ca>
2024-11-05 19:14:58 -0800

    Potentially working no ground truth

--- src/hector_ui/src/ui_hector_quad.py
+++ src/hector_ui/src/ui_hector_quad.py
@@ -2,8 +2,9 @@
 #  -*- coding: utf-8 -*-
 from tkinter import ttk
 from std_msgs.msg import Empty
-from geometry_msgs.msg import Twist, PoseStamped
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+from hector_uav_msgs.msg import Altimeter
 import tkinter as tk
 import math
 import rospy
@@ -11,38 +12,41 @@ import rospy
 root = tk.Tk(className="Hector Quadrotor UI")
 root.title("Hector Quadrotor UI")
 
-x_p = tk.StringVar()
-y_p = tk.StringVar()
 z_p = tk.StringVar()
 z_o = tk.StringVar()
 
-def quaterionToRads(data):
-    x = data.pose.orientation.x
-    y = data.pose.orientation.y
-    z = data.pose.orientation.z
-    w = data.pose.orientation.w
+def quaternion_to_yaw(orientation):
+    """
+    Converts quaternion orientation to yaw.
+    :param orientation: A quaternion object from Imu topic
+    """
+    x = orientation.x
+    y = orientation.y
+    z = orientation.z
+    w = orientation.w
     t3 = 2.0 * (w * z + x * y)
     t4 = 1.0 - 2.0 * (y * y + z * z)
     yawZActual = math.atan2(t3, t4)
     if yawZActual < 0:
-        yawZActual = 2*math.pi + yawZActual
+        yawZActual = 2 * math.pi + yawZActual
     return yawZActual
 
-# Callback for simulator position and orientation
-def pose_callback(data):
-    x_p.set("{0:.2f}".format(data.pose.pose.position.x))
-    y_p.set("{0:.2f}".format(data.pose.pose.position.y))
-    z_p.set("{0:.2f}".format(data.pose.pose.position.z))
+# Callback for altitude from altimeter
+def altimeter_callback(data):
+    # Using altitude field from Altimeter message
+    z_p.set("{0:.2f}".format(data.altitude))
 
-def rot_callback(data):
-    z_o.set("{0:.2f}".format(math.degrees(quaterionToRads(data))))
+# Callback for yaw orientation from IMU
+def imu_callback(data):
+    yaw = quaternion_to_yaw(data.orientation)
+    z_o.set("{0:.2f}".format(math.degrees(yaw)))
 
 # ROS initialization
 rospy.init_node('HectorQ_GUI', anonymous=False)
 
 # Subscribers
-posicionLider_sub = rospy.Subscriber("/quad/ground_truth/state", Odometry , pose_callback)
-orientaLider_sub = rospy.Subscriber("/quad/ground_truth_to_tf/pose", PoseStamped , rot_callback)
+altimeter_sub = rospy.Subscriber("/quad/altimeter", Altimeter, altimeter_callback)
+imu_sub = rospy.Subscriber("/quad/raw_imu", Imu, imu_callback)
 
 # Publishers
 takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
@@ -50,7 +54,7 @@ land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=1)
 vel_pub = rospy.Publisher('/quad/cmd_vel', Twist, queue_size=1)
 
 # Movement functions
-def enviar_velocidad(vx,vy,vz,vaz):
+def enviar_velocidad(vx, vy, vz, vaz):
     vel_msg = Twist()
     vel_msg.linear.x = float(vx)
     vel_msg.linear.y = float(vy)
@@ -59,7 +63,7 @@ def enviar_velocidad(vx,vy,vz,vaz):
     vel_pub.publish(vel_msg)
 
 def hover_pub():
-    enviar_velocidad(0.0,0.0,0.0,0.0)
+    enviar_velocidad(0.0, 0.0, 0.0, 0.0)
 
 def takeoff_fun():
     takeoff_pub.publish(Empty())
@@ -67,14 +71,14 @@ def takeoff_fun():
 def land_fun():
     land_pub.publish(Empty())
 
-def up_fun(): enviar_velocidad(0.0,0.0,0.5,0.0)
-def down_fun(): enviar_velocidad(0.0,0.0,-0.5,0.0)
-def forward_fun(): enviar_velocidad(0.5,0.0,0.0,0.0)
-def backward_fun(): enviar_velocidad(-0.5,0.0,0.0,0.0)
-def right_fun(): enviar_velocidad(0.0,-0.5,0.0,0.0)
-def left_fun(): enviar_velocidad(0.0,0.5,0.0,0.0)
-def cw_fun(): enviar_velocidad(0.0,0.0,0.0,-0.5)
-def ccw_fun(): enviar_velocidad(0.0,0.0,0.0,0.5)
+def up_fun(): enviar_velocidad(0.0, 0.0, 0.5, 0.0)
+def down_fun(): enviar_velocidad(0.0, 0.0, -0.5, 0.0)
+def forward_fun(): enviar_velocidad(0.5, 0.0, 0.0, 0.0)
+def backward_fun(): enviar_velocidad(-0.5, 0.0, 0.0, 0.0)
+def right_fun(): enviar_velocidad(0.0, -0.5, 0.0, 0.0)
+def left_fun(): enviar_velocidad(0.0, 0.5, 0.0, 0.0)
+def cw_fun(): enviar_velocidad(0.0, 0.0, 0.0, -0.5)
+def ccw_fun(): enviar_velocidad(0.0, 0.0, 0.0, 0.5)
 
 # Bind keys for keyboard control
 root.bind('<KeyPress-i>', lambda e: forward_fun())
@@ -92,17 +96,13 @@ mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 mainframe.columnconfigure(0, weight=1)
 
-ttk.Label(mainframe, textvariable=x_p).grid(column=1, row=2, sticky=(tk.W, tk.E))
-ttk.Label(mainframe, textvariable=y_p).grid(column=2, row=2, sticky=(tk.W, tk.E))
-ttk.Label(mainframe, textvariable=z_p).grid(column=3, row=2, sticky=(tk.W, tk.E))
-ttk.Label(mainframe, textvariable=z_o).grid(column=4, row=2, sticky=(tk.W, tk.E))
+ttk.Label(mainframe, textvariable=z_p).grid(column=1, row=2, sticky=(tk.W, tk.E))
+ttk.Label(mainframe, textvariable=z_o).grid(column=2, row=2, sticky=(tk.W, tk.E))
 
-ttk.Label(mainframe, text="X (m)").grid(column=1, row=3, sticky=tk.W)
-ttk.Label(mainframe, text="Y (m)").grid(column=2, row=3, sticky=tk.W)
-ttk.Label(mainframe, text="Z (m)").grid(column=3, row=3, sticky=tk.W)
-ttk.Label(mainframe, text="Yaw (°)").grid(column=4, row=3, sticky=tk.W)
+ttk.Label(mainframe, text="Altitude (m)").grid(column=1, row=3, sticky=tk.W)
+ttk.Label(mainframe, text="Yaw (°)").grid(column=2, row=3, sticky=tk.W)
 
-ttk.Button(mainframe, text="Take Off", command=takeoff_fun).grid(column=3, row=4, sticky=tk.W)
+ttk.Button(mainframe, text="Take Off", command=takeoff_fun).grid(column=1, row=4, sticky=tk.W)
 ttk.Button(mainframe, text="Land", command=land_fun).grid(column=2, row=4, sticky=tk.W)
 
 for child in mainframe.winfo_children(): 
