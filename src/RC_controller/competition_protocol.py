049c26230922f81ac74847c0ba500fb390e87f6c
Rylan Stutters <ryds105@gmail.com>
2024-11-15 17:36:18 -0800

    Comp protocol imports

--- src/RC_controller/competition_protocol.py
+++ src/RC_controller/competition_protocol.py
@@ -11,6 +11,8 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'nodes'))
 from sensor_msgs.msg import Image
 from sign_reader import SignReader
 from character_recognition import OCRNode
+from clue_verification import ClueVerifier
+from clue_publisher import CluePublisher
 
 def main():
     # Create an instance of the SignReader class and start it
@@ -19,9 +21,11 @@ def main():
 
     # TO BE TESTED #
     # Create an instance of the OCR class and display latest image
-    # parser = OCRNode()
-    # cv2.imshow("Sign", parser.latest_image)
-    # cv2.waitKey(1)
+    parser = OCRNode()
+    cv2.imshow("Sign", parser.latest_image)
+    cv2.waitKey(1)
+
+
 
 
 if __name__ == '__main__':
