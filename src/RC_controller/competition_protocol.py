024652b2d9b4df38137eea1d9fdd11e17aad8179
Rylan Stutters <ryds105@gmail.com>
2024-11-17 19:44:23 -0800

    Startup Procedure

--- src/RC_controller/competition_protocol.py
+++ src/RC_controller/competition_protocol.py
@@ -4,20 +4,26 @@ import rospy
 import cv2
 import sys
 from sensor_msgs.msg import Image
+from std_msgs.msg import Int8
+from std_msgs.msg import String
 
 class CompProtocol:
     def __init__(self):
         rospy.init_node('competition_protocol_node')
         self.pub = rospy.Publisher('/state', String, queue_size=10)
-        rospy.Subscriber('/clue_count', Int, self.clue_count_callback)
+        rospy.Subscriber('/clue_count', Int8, self.clue_count_callback)
 
         self.clue_count = 0
 
     def clue_count_callback(self, count):
-        self.clue_count = count
+        self.clue_count = count.data
 
     def run(self):
+        rospy.sleep(1)
         self.pub.publish("STARTUP")
+        rospy.loginfo("Published STARTUP state to /state")
+
+        rospy.spin()
 
 
 if __name__ == '__main__':
--- src/RC_controller/nodes/clue_publisher.py
+++ src/RC_controller/nodes/clue_publisher.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 from std_msgs.msg import String
+from std_msgs.msg import Int8
 
 class CluePublisher:
     def __init__(self):
@@ -16,7 +17,15 @@ class CluePublisher:
         self.pub = rospy.Publisher('/score_tracker', String, queue_size=10)
 
         # Subscribe to the OCR output
-        rospy.Subscriber('/verified_clues', String, queue_size=10)
+        rospy.Subscriber('/verified_clues', String, self.verified_clues_callback)
+
+        # Subscribe to the State output
+        rospy.Subscriber('/state', String, self.state_callback)
+
+        # Subscribe to the Clue Count output
+        rospy.Subscriber('/clue_count', Int8, self.clue_count_callback)
+
+        self.clue_count = 0
 
 
     def start_comp(self):
@@ -29,6 +38,16 @@ class CluePublisher:
         number = str(clue)
         answer = str(answer)
         self.pub.publish(f'TeamRC,funky,{number},{answer}')
+
+    def clue_count_callback(self, count):
+        self.clue_count = count.data
+
+    def verified_clues_callback(self, clue):
+        self.submit_clue(clue, self.clue_count)
+
+    def state_callback(self, state):
+        if state.data == "STARTUP":
+            self.start_comp()
 
     def start(self):
         # Start the ROS loop
