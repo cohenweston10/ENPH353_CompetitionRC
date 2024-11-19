c110ed66952c88a4eebc5794a20a13e791f0430a
Rylan Stutters <ryds105@gmail.com>
2024-11-18 21:33:17 -0800

    Added respawn

--- src/RC_controller/competition_protocol.py
+++ src/RC_controller/competition_protocol.py
@@ -6,6 +6,9 @@ import sys
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int8
 from std_msgs.msg import String
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
+import numpy as np
 
 class CompProtocol:
     def __init__(self):
@@ -17,6 +20,29 @@ class CompProtocol:
 
     def clue_count_callback(self, count):
         self.clue_count = count.data
+
+    def spawn_position(self, position):
+        msg = ModelState()
+        msg.model_name = 'quadrotor'
+
+        msg.pose.position.x = position[0]
+        msg.pose.position.y = position[1]
+        msg.pose.position.z = position[2]
+        msg.pose.orientation.x = position[3]
+        msg.pose.orientation.y = position[4]
+        msg.pose.orientation.z = position[5]
+        msg.pose.orientation.w = position[6]
+
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            resp = set_state( msg )
+
+        except rospy.ServiceException:
+            print ("Service call failed")
+
+    def respawn(self):
+        self.spawn_position([5.52, 2.25, 0.06, 0, 0, np.sin(-np.pi/4), np.cos(-np.pi/4)])
 
     def run(self):
         rospy.sleep(1)
@@ -31,7 +57,7 @@ class CompProtocol:
         self.pub.publish("STOP")
         rospy.loginfo("Published STOP state to /state")
 
-        rospy.spin()
+        self.respawn()
 
 
 if __name__ == '__main__':
