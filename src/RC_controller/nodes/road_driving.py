6f45d0573518241972e8be247babce02947aab2c
Rylan Stutters <ryds105@gmail.com>
2024-11-19 12:59:01 -0800

    Added side swapping

--- src/RC_controller/nodes/road_driving.py
+++ src/RC_controller/nodes/road_driving.py
@@ -3,7 +3,7 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
+from std_msgs.msg import String, Int8
 
 class RoadDriving:
     def __init__(self):
@@ -15,10 +15,19 @@ class RoadDriving:
         # Subscribe to the State output
         rospy.Subscriber('/state', String, self.state_callback)
 
+        # Subscribe to the Clue Count output
+        rospy.Subscriber('/clue_count', Int8, self.clue_count_callback)
+
         self.state = ""
+        self.clue_count = 0
+        self.switch_pending = True
 
     def state_callback(self, state):
         self.state = state.data
+
+    def clue_count_callback(self, count):
+        self.clue_count = count.data
+        self.switch_pending = True
 
     @staticmethod
     def quaternion_to_yaw(orientation):
@@ -54,16 +63,41 @@ class RoadDriving:
     def operate(self):
         """Main loop to check state and drive accordingly."""
         while not rospy.is_shutdown():
-            if self.state == "DRIVING":
-                ######
-                #
-                # Implement PID here
-                #
-                ######
-                self.update_velocity(1, 0, 0, 0)
+            if self.state == "STARTUP":
+                self.update_velocity(0,0,1,0)
+
+            elif self.state == "DRIVING":
+                if self.clue_count == 0:
+                    if self.switch_pending == True:
+                        rospy.loginfo("Swapping Left")
+                        self.sideSwap("ToLeft")
+                        self.switch_pending = False
+                    self.lineFollow()
+                elif self.clue_count == 1:
+                    if self.switch_pending == True:
+                        rospy.loginfo("Swapping Right")
+                        self.sideSwap("ToRight")
+                        self.switch_pending = False
+                    self.lineFollow()
+
+
             elif self.state == "STOP":
                 self.update_velocity(0, 0, 0, 0)
             rospy.sleep(0.1)  # Sleep for a short time to avoid high CPU usage
+
+
+    def sideSwap(self, direction):
+        if direction == "ToLeft":
+            self.update_velocity(0,0.5,0,0)
+            rospy.sleep(1)
+            self.update_velocity(0, 0, 0, 0)
+        elif direction == "ToRight":
+            self.update_velocity(0,-0.5,0,0)
+            rospy.sleep(1)
+            self.update_velocity(0, 0, 0, 0)
+
+    def lineFollow(self):
+        self.update_velocity(0,0,0,0)
 
     def start(self):
         # Start the ROS loop
